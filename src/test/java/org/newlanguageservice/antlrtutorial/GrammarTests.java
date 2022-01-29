cba54d1689394f35596dd556e67f20dfa8c6f724
Vladimir Kozhaev <vkozhaev@gmail.com>
2022-01-30 02:59:25 +0300

    Add a points equation test

--- src/test/java/org/newlanguageservice/antlrtutorial/GrammarTests.java
+++ src/test/java/org/newlanguageservice/antlrtutorial/GrammarTests.java
@@ -7,15 +7,26 @@ import org.junit.*;
 public class GrammarTests {
 	@Test
 	public void simpleGrammarTests() {
-		assertEquals(0,new DrawCanvas().processALanguage(null, "MoveTo(0,0)\nLineTo(1,1)").getExceptions().size());
+		assertEquals(0, new DrawCanvas().processALanguage(null, "MoveTo(0,0)\nLineTo(1,1)").getExceptions().size());
 	}
+
 	@Test
 	public void testOfAddingAVariable() {
-		assertEquals(0,new DrawCanvas().processALanguage(null, "var t: int= 5 + 5 ").getExceptions().size());
+		assertEquals(0, new DrawCanvas().processALanguage(null, "var t: int= 5 + 5 ").getExceptions().size());
 	}
-	
+
 	@Test
 	public void testToMoveWithVarRef() {
-		assertEquals(0,new DrawCanvas().processALanguage(null, "var x:int = 1 MoveTo(x,x)").getExceptions().size());
+		assertEquals(0, new DrawCanvas().processALanguage(null, "var x:int = 1 MoveTo(x,x)").getExceptions().size());
+	}
+
+	@Test
+	public void testSimplePointEquation() {
+		assertEquals(0, new DrawCanvas().processALanguage(null, "var x:Point = (1,1) +(2,2)").getExceptions().size());
+	}
+
+	@Test(expected = LanguageException.class)
+	public void testAnExceptionInPointEquation() {
+		assertEquals(0, new DrawCanvas().processALanguage(null, "var x:Point = (1,1) * (2,2)").getExceptions().size());
 	}
 }
